635751039b46368990a52c10b3bfce2c53432ec1
Harrison <h.alexander.barnett@gmail.com>
2021-07-30 15:40:12 -0400

    Pass values from addtask input fields

--- to-do/src/Content.js
+++ to-do/src/Content.js
@@ -10,14 +10,41 @@ class Content extends React.Component {
             group: "work"
         }      
         this.handleTaskSubmit = this.handleTaskSubmit.bind(this);
-    }
-
-    handleTaskSubmit() {
+        this.handleContent = this.handleContent.bind(this);
+        this.handleDate = this.handleDate.bind(this);
+        this.handleGroup = this.handleGroup.bind(this);
+    };
+    handleContent(event) {
+        this.setState({content: event.target.value});
+    };
+    handleDate(event) {
+        this.setState({date: event.target.value});
+    };
+    handleGroup(event) {
+        this.setState({group: event.target.value});
+    };
+    handleTaskSubmit(event) {
+        event.preventDefault();
         this.props.addTask(this.state.id, this.state.content, this.state.date, this.state.group);
     }
     render() {
         return (
             <div className="content-container">
+                <input type="text"
+                onChange={this.handleContent}>
+
+                </input>
+                <input type="date"
+                onChange={this.handleDate}>
+                </input>
+
+                <select 
+                name="add-task-groups-dropdown" 
+                onChange={this.handleGroup}>
+                    <option value="work">Work</option>
+                    <option value="school">School</option>
+                </select>
+
                 <button onClick={this.handleTaskSubmit}>
                     click me
                 </button>

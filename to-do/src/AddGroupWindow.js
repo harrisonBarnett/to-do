470d5f3815c9f88c9bdfca40e86d6332dcbaf3d9
Harrison <h.alexander.barnett@gmail.com>
2021-08-03 00:09:26 -0400

    whatever

--- to-do/src/AddGroupWindow.js
+++ to-do/src/AddGroupWindow.js
@@ -1,51 +1,62 @@
 import React from "react";
 
-const AddGroupWindow = ({closeBtn, show}) => {
+class AddGroupWindow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-    const modalClassName = show ? "add-group-window display-block" : "add-group-window display-none";
+        }
+    };
 
-    return (
-        <div
-        className={modalClassName}
-        onClick={closeBtn}>
-            <div className="add-group-window-content"
-            onClick="none">
+    render() {
+        const { show, closeBtn } = this.props;
 
-                <div id="add-group-form">
-                    <h2>form a group</h2>
-                    <div id="group-input-container">
-                        <label htmlFor="group-input">group: 
-                            <input 
-                            type="text"
-                            name="group-input"/>
-                        </label>
+        const modalClassName = show ? "add-group-window display-block" : "add-group-window display-none";
+
+        return (
+            <div
+            className={modalClassName}
+            onClick={closeBtn}>
+                <div className="add-group-window-content"
+                onClick="none">
+    
+                    <div id="add-group-form">
+                        <h2>form a group</h2>
+                        <div id="group-input-container">
+                            <label htmlFor="group-input">group: 
+                                <input 
+                                type="text"
+                                name="group-input"/>
+                            </label>
+                        </div>
+                        <div id="color-input-container">
+                            <label htmlFor="color-input">color: 
+                                <input 
+                                type="color"
+                                name="color-input"/>
+                            </label>
+                        </div>  
                     </div>
-                    <div id="color-input-container">
-                        <label htmlFor="color-input">color: 
-                            <input 
-                            type="color"
-                            name="color-input"/>
-                        </label>
-                    </div>  
-                </div>
-
-                <div id="add-group-window-control">
-                    <button
-                    type="button"
-                    id="add-group-close-btn"
-                    onClick={closeBtn}>
-                        x
-                    </button>
-
-                    <button
-                    type="button"
-                    id="add-group-submit-button"
-                    onClick={closeBtn}>
-                        submit
-                    </button>
+    
+                    <div id="add-group-window-control">
+                        <button
+                        type="button"
+                        id="add-group-close-btn"
+                        onClick={closeBtn}>
+                            x
+                        </button>
+    
+                        <button
+                        type="button"
+                        id="add-group-submit-button"
+                        onClick={() => closeBtn}>
+                            submit
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 export default AddGroupWindow;
